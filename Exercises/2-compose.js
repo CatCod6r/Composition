f84c724d65840ed814ac188b26d97f86f971321c
CatCod6r <165474293+CatCod6r@users.noreply.github.com>
2024-12-02 14:02:40 +0000

    Solve 2-compose.js

--- Exercises/2-compose.js
+++ Exercises/2-compose.js
@@ -1,5 +1,29 @@
 'use strict';
 
-const compose = (...fns) => (x) => null;
+const combine = (...functions) => (input) => {
+  const callbacks = [];
+  const combined = (input) => {
+    if (functions.length === 0) return input;
+    const len = functions.length - 1;
+    let result = input;
+    try {
+      for (let i = len; i >= 0; i--) {
+        result = functions[i](result);
+      }
+      return result;
+    } catch (err) {
+      for (const callback of callbacks) {
+        callback(err);
+      }
+      return null;
+    }
+  };
+  combined.on = (event, callback) => {
+    if (event === 'error') callbacks.push(callback);
+  };
+  return combined;
+};
+
+
 
 module.exports = { compose };
